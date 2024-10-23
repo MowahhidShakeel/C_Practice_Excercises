b576e654aa33dc0a92f6fadb9df39d4d741ca62f
MowahhidShakeel <mowahhidtech@gmail.com>
2024-10-23 20:02:49 +0500

    Completed first question of chapter 2

--- Chapter2/q2.1.c
+++ Chapter2/q2.1.c
@@ -1,9 +1,27 @@
 #include <float.h>
 #include <limits.h>
 #include <stdio.h>
+#include <math.h>
+
+#define ZERO 0
 
 int main()
 {
-    printf("HELLO");
-    return 0;
+    /* Solved by directly printing the values of the header files */
+    
+    printf("Range of signed char: %d to %d\n", SCHAR_MIN, SCHAR_MAX);
+    printf("Range of unsigned char: %d to %d\n", ZERO, UCHAR_MAX);
+    printf("\n");
+    
+    printf("Range of signed short: %d to %d\n", SHRT_MIN, SHRT_MAX);
+    printf("Range of unsigned short: %d to %d\n", ZERO, USHRT_MAX);
+    printf("\n");
+    
+    printf("Range of signed int: %d to %d\n", INT_MAX, INT_MIN);
+    printf("Range of unsigned short: %d to %d\n", ZERO, UINT_MAX);
+    printf("\n");
+    
+    printf("Range of signed long: %ld to %ld\n", LONG_MIN, LONG_MAX);
+    printf("Range of unsigned long: %lu to %lu\n", ZERO, ULONG_MAX);
+
 }
